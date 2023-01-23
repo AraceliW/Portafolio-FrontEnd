672e2c0b12e2b7ffa449ba85d6c373c575bec111
AraceliW <araceli.lopez160101@gmail.com>
2023-01-22 23:16:25 -0300

    Creacion de la seccion acerca-de

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    HeaderComponent,
+    AcercaDeComponent,
   ],
   imports: [
     BrowserModule
