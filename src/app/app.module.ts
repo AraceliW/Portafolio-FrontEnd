49a42faf501a82b8b517a97393dd6ba2202a538e
AraceliW <araceli.lopez160101@gmail.com>
2023-01-24 17:22:45 -0300

    Se agrego la seccion experiencia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
     NavbarComponent,
     HeaderComponent,
     AcercaDeComponent,
+    ExperienciaComponent,
   ],
   imports: [
     BrowserModule
